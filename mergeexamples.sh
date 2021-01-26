a5933a4310295837e2369d51d9acb3cbd33a18b0
Jürgen Reichmann <j.reichmann@rdvsb.de>
2021-01-26 10:29:53 +0100

    genesis

---
+++ mergeexamples.sh
@@ -0,0 +1,4 @@
+meld ./kmUtilCommonExample/src/commonMain/kotlin/main.kt ./kmUtilLinuxExample/src/nativeMain/kotlin/main.kt
+meld ./kmUtilLinuxExample/src/nativeMain/kotlin/main.kt ./kmUtilMingwExample/src/nativeMain/kotlin/main.kt
+meld ./kmUtilCommonExample/src/commonMain/kotlin/main.kt ./kmKScriptExample/scriptKmUtil.kt
+meld ./kmKScriptExample/scriptKmUtil.kt ./kmKScriptExample/scriptKmUtil.kts
