49db0bd31cf59d8b1ba64b1daf8c937f1f258863
Jürgen Reichmann <j.reichmann@rdvsb.de>
2021-01-26 10:27:05 +0100

    refactored main class GetArgs

--- kmUtilCommonExample/src/commonMain/kotlin/main.kt
+++ kmUtilCommonExample/src/commonMain/kotlin/main.kt
@@ -1,35 +1,102 @@
 import de.rdvsb.kmapi.*
 import de.rdvsb.kmutil.*
-import de.rdvsb.kmutil.logMessage
-import de.rdvsb.kmutil.logPath
-import kotlin.time.Duration
 
-//import de.rdvsb.kmutil.KmLog
-
-
-fun main(args: Array<String>) {
-
-	System.err.println("asd")
-	val processBuilder = ProcessBuilder("ls")
-	processBuilder.redirectInput(ProcessBuilder.Redirect.INHERIT)
-
-	val process = processBuilder.start()
-	val inp: InputStream = process.inputStream
-
-	val timeout: Duration = Duration.INFINITE
-
-	val l = inp.bufferedReader().readLines()
-	println(l)
-	if (timeout.isFinite()) {
-		process.waitFor(timeout.toLongMilliseconds(), TimeUnit.MILLISECONDS)
-	} else {
-		process.waitFor()
+/**
+ * parse arguments and store the argument values
+ *
+ * extend BasicGetArgs class from scriptUtil.kt to build real getArgs object
+ */
+class GetArgs : BasicGetArgs() {
+	init {
+		appPath = updateAppPath(this)
+		basicGetArgs = this // overwrite basicGetArgs initialized with empty placeholder in scriptUtil.kt with the real one
 	}
 
+	val MINNAMES = 0
+	val MAXNAMES = 300
+	var isSimulate = false
+	val isNotSimulate get() = !isSimulate
 
+	var isTest = false
+	val isNotTest get() = !isTest
+	var dir = ""
+	val names = mutableListOf<String>()
+
+	/**
+	 * parse an [Array] of [String] as commandline parameters and set properties accordingly.
+	 *
+	 * on errors call [usage] () and exit with rc 9
+	 */
+	operator fun invoke(args: Array<String>) {
+		if (args.size < MINNAMES) usage()
+		args.asList().iterator().apply args@{
+			for (arg in this) {
+				when (arg) {
+					// handle long args first
+					"--help"     -> usage()
+					"--simulate" -> isSimulate = true
+					"--verbose"  -> isVerbose = true
+					"--quiet"    -> isQuiet = true
+					"--test"     -> isTest = true
+					"--debug"    -> debugLevel++
+					"--dir"      -> dir = if (hasNext()) next() else usage("$arg needs a parameter")
+					"-", ""      -> names += arg
+					"--"         -> names.addAll(asSequence()) // append remaining elements
+					else         -> {
+						if (arg.isNotEmpty() && arg[0] == '-') {
+							// handle short args here
+							arg.forEachIndexed { i, ch ->
+								i == 0 && return@forEachIndexed
+								i == 1 && ch == '-' && usage("unknown long argument \"$arg\"")
+								when (ch) {
+									'h', 'u' -> usage()
+									'd'      -> ++debugLevel
+									's'      -> isSimulate = true
+									'q'      -> isQuiet = true
+									'v'      -> isVerbose = true
+									else     -> usage("unknown short argument char \"$ch\" in argument token \"$arg\"")
+								}
+							}
+						} else {
+							names.add(arg)
+						}
+					}
+				}
+			}
+		}
+
+		if (names.size < MINNAMES) usage("no names supplied")
+		if (names.size > MAXNAMES) usage("too many arguments")
+		logMessage.isQuiet = isQuiet
+	}
+
+	fun usage(errText: String? = null): Nothing {
+		errText?.let { System.err.println("+++ $errText") }
+
+		System.err.println(
+			"""
+			|usage1 $appName [--debug] [--quiet|--verbose] [--dir <name>] [--] name*
+			|  do something with names
+			|  --dir: optional dir
+			|  -- stops argument parsing and adds remaining tokens as names
+		""".trimMargin()
+		)
+
+		exit(9)
+	}
+}
+
+var getArgs = GetArgs()
+
+fun main(args: Array<String>) {
+	getArgs(args)
+	logMessage.maxLogSize = 1
+	// switch of logging to file: logMessage.logName = ""
+
+	logMessage('I', "kmutil-jvm example start (${System.getProperty("app.name")} on ${System.getProperty("os.name")})")
+	logMessage('I', "dir=${getArgs.dir}, names=${getArgs.names.map { "\"$it\"" }}, isDebug=${getArgs.isDebug}")
 
 	val f = File("/tmp/.x.x")
-
 
 	//throw (IOException("Test"))
 
@@ -47,5 +114,6 @@ fun main(args: Array<String>) {
 //
 //	println("renameTo=${f.renameTo(KmFile("/tmp/x.y"))}, newPath=${f.path}")
 
+	logMessage('I', "kmutil-jvm example end")
 
 }
